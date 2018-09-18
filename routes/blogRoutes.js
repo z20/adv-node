0792e2d4c4d3625171c86e771228adfff045f7a4
Zach Holdren <zholdren20@gmail.com>
2018-09-17 19:42:46 -0700

    initial commit

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,9 +14,25 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const redisURL = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisURL);
+    const util = require('util');
+    client.get = util.promisify(client.get);
+
+    const cachedBlogs = await client.get(req.user.id);
+
+    if (cachedBlogs) {
+      console.log('Serving from cache...');
+      return res.send(JSON.parse(cachedBlogs));
+    }
+
     const blogs = await Blog.find({ _user: req.user.id });
 
+    console.log('Serving from MongoDB...');
     res.send(blogs);
+
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
